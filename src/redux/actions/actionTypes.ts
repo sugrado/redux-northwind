f3614c1fbe29628ae872f03d08ad99ad31caa48e
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-02-09 17:29:51 +0300

    Text Input component and product post/put actions

--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -2,6 +2,8 @@ export const CHANGE_CATEGORY: string = "CHANGE_CATEGORY";
 export const GET_CATEGORIES_SUCCESS: string = "GET_CATEGORIES_SUCCESS";
 
 export const GET_PRODUCTS_SUCCESS: string = "GET_PRODUCTS_SUCCESS";
+export const CREATE_PRODUCTS_SUCCESS: string = "CREATE_PRODUCTS_SUCCESS";
+export const UPDATE_PRODUCTS_SUCCESS: string = "UPDATE_PRODUCTS_SUCCESS";
 
 export const ADD_TO_CART: string = "ADD_TO_CART";
 export const REMOVE_FROM_CART: string = "REMOVE_FROM_CART";
--- src/redux/actions/productAction.ts
+++ src/redux/actions/productAction.ts
@@ -1,13 +1,31 @@
 import axios from "axios";
 import { Action } from "../../models/Action";
 import { Product } from "../../models/Product";
-import { GET_PRODUCTS_SUCCESS } from "./actionTypes";
+import {
+  CREATE_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_SUCCESS,
+  UPDATE_PRODUCTS_SUCCESS,
+} from "./actionTypes";
 
 export function getProductsSuccess(products: Product[]) {
   return {
     type: GET_PRODUCTS_SUCCESS,
     payload: products,
   } as Action<Product[]>;
+}
+
+export function createProductSuccess(product: Product) {
+  return {
+    type: CREATE_PRODUCTS_SUCCESS,
+    payload: product,
+  } as Action<Product>;
+}
+
+export function updateProductSuccess(product: Product) {
+  return {
+    type: UPDATE_PRODUCTS_SUCCESS,
+    payload: product,
+  } as Action<Product>;
 }
 
 export function getProducts(categoryId: number | undefined = undefined) {
@@ -17,4 +35,36 @@ export function getProducts(categoryId: number | undefined = undefined) {
       .then((response) => dispatch(getProductsSuccess(response.data)))
       .catch((error) => console.log(error));
   };
+}
+
+export function saveProduct(product: Product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductSuccess(savedProduct))
+          : dispatch(createProductSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveProductApi(product: Product) {
+  let method = product.id
+    ? axios.put("products", product)
+    : axios.post("products", product);
+
+  return method.then(handleResponse).catch(handleError);
+}
+
+export async function handleResponse(response: any) {
+  if (response.ok) return response;
+  const error = await response;
+  throw new Error(error);
+}
+
+export async function handleError(error: any) {
+  throw error;
 }
